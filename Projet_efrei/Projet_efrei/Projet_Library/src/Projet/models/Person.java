84fa5272e5eff1a69ddfd99517e2352c1e7c50d0
Grimyolf <95622304+Grimyolf@users.noreply.github.com>
2021-12-14 13:57:34 +0100

    Commit 5

--- Projet_efrei/Projet_efrei/Projet_Library/src/Projet/models/Person.java
+++ Projet_efrei/Projet_efrei/Projet_Library/src/Projet/models/Person.java
@@ -1,5 +1,84 @@
 package Projet.models;
 
+import java.sql.Date;
+
 public class Person {
+	
+	private int personId;
+	private String name;
+	private String lastName;
+	private String password;
+	private Date dateOfBirth;
+	private int adressId;
+	private String phone;
+	private String genre;
+
+	public Person(String name, String lastName, String synopsis, String genre, String is_issued) {
+		this.setName(name);
+		this.setAuthor(lastName);
+		this.setSynopsis(password);
+		this.setGenre(genre);
+		this.setIS_issued(is_issued);
+	}
+
+	public Person (int id, String name, String lastName, String password, String genre, String is_issued) {
+		this(name, lastName, password, genre, is_issued);
+		this.setId(id);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return name;
+	}
+
+	public void setTitle(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getIS_issued() {
+		return synopsis;
+	}
+
+	public void setIS_issued(String is_issued) {
+		this.is_issued = is_issued;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", synopsis=" + synopsis + ", genre=" + genre +", disponible =" + is_issued +"]";
+	}
 
 }
+
+
--- Projet_efrei/Projet_efrei/Projet_Library/src/Projet/repositories/Repo_person.java
+++ Projet_efrei/Projet_efrei/Projet_Library/src/Projet/repositories/Repo_person.java
@@ -1,5 +1,58 @@
 package Projet.repositories;
 
-public class Repo_person {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import Projet.DbConnection;
+import Projet.abstracts.DAOImpl;
+import Projet.models.Person;
+
+public class Repo_person extends DAOImpl<Person> {
+
+	public Persons(DbConnection conn) {
+			super(conn, "Persons");
+		}
+
+		@Override
+		protected Person fromResultSet(ResultSet set) throws SQLException {
+			int id = set.getInt("Id");
+			String name = set.getString("Name");
+			String lastName = set.getString("LastName");
+			return new Person(id, name, lastName, password, dateOfBirth, adressId, phone, genre);
+		}
+
+		@Override
+		public boolean add(Person person) {
+			String sql = String.format("Insert into %s(Name, LastName) values(?,?)", tableName);
+			try {
+				PreparedStatement preparedStatement = this.conn.createPreparedStatement(sql);
+				preparedStatement.setString(1, person.getName());
+				preparedStatement.setString(2, person.getLastName());
+				preparedStatement.execute();
+				return true;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return false;
+		}
+
+		@Override
+		public boolean update(int id, Person person) {
+			String sql = String.format("UPDATE %s SET Name = ?, LastName=? Where Id = ?", tableName);
+			try {
+				PreparedStatement preparedStatement = this.conn.createPreparedStatement(sql);
+				preparedStatement.setString(1, person.getName());
+				preparedStatement.setString(2, person.getLastName());
+				preparedStatement.setInt(3, id);
+				preparedStatement.execute();
+				return true;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return false;
+		}
+
+	
+	
 }
