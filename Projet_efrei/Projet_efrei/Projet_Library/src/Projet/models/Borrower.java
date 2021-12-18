170ef70bb26ec187ef6024e6351c7cc3c2824031
Mallow <64898654+MallowBot@users.noreply.github.com>
2021-12-18 17:36:15 +0100

    Update Borrower.java
    Borrower Model

--- Projet_efrei/Projet_efrei/Projet_Library/src/Projet/models/Borrower.java
+++ Projet_efrei/Projet_efrei/Projet_Library/src/Projet/models/Borrower.java
@@ -1,5 +1,49 @@
 package Projet.models;
 
 public class Borrower {
+	private int id;
+	private String name;
+	private String lastname;
 
+	public Borrower(String name, String lastname) {
+		this.setName(name);
+		this.setLastName(lastname);
+	}
+	
+	public Borrower(int id, String name, String lastname) {
+		this(name, lastname);
+		this.setId(id);
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	@Override
+	public String toString() {
+		return "Borrower [id=" + id + ", name=" + name + ", lastname=" + lastname + "]";
+	}
 }
